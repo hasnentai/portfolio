067d20a692df2f4be15ff754f0dec4b4d49eb974
sumanaghosh15 <sumanaghosh822@gmail.com>
2021-11-08 19:03:16 +0530

    look at the progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './banner/banner';
 import Header from './header';
+import Projects from './projects/projects';
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <Projects />
     </>
 
   );
