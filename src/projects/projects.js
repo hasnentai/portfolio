8e1400ffe5fa5f198c16ff6ffa709ac5197ea060
sumanaghosh15 <sumanaghosh822@gmail.com>
2021-11-09 16:50:03 +0530

    project section done

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,6 +1,7 @@
 import './projects.css';
 import firstProjectImg from '../assets/proImg1.png';
 import secondProjectImg from '../assets/proImg2.png';
+import thirdProjectImg from '../assets/proImg3.png';
 
 function Projects() {
     return (
@@ -26,6 +27,22 @@ function Projects() {
                     <div className="vector4"></div>
                     <div className="vector5"></div>
                 </div>
+                <div className="blurBottom2"></div>
+            </div>
+
+            <div className="secondProject">
+                <div className="secondProjectRight">
+                    <img src={thirdProjectImg} alt="secondProjectImage" />
+                    <div className="vector6"></div>
+                    <div className="vector7"></div>
+                </div>
+                <div className="secondProjectLeft">
+                    <p className="proTitle">Project title</p>
+                    <p className="mobileApp">Mobile App</p>
+                    <p className="proDes">You can add what outcomes has this project brought after your design! For example, can show some real data.</p>
+                    <p className="tellMeMore">Tell me more</p>
+                </div>
+
                 <div className="blurBottom2"></div>
             </div>
         </div>
