a849d76fe2a76806e5379d4b754a8cbf759d899e
sumanaghosh15 <sumanaghosh822@gmail.com>
2021-11-09 16:03:09 +0530

    some changes made in project section 2

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -11,7 +11,7 @@ function Projects() {
                 <p className="proDescription">You can add what outcomes has this project brought after your design! For example, can show some real data.</p>
                 <p className="tellMeMore">Tell me more</p>
                 <img src={firstProjectImg} alt="firstProjectImage" />
-                <div className="vector"></div>
+                <div className="vector3"></div>
                 <div className="blurBottom"></div>
             </div>
             <div className="secondProject">
@@ -23,8 +23,8 @@ function Projects() {
                 </div>
                 <div className="secondProjectRight">
                     <img src={secondProjectImg} alt="secondProjectImage" />
-                    <div className="vector2"></div>
-                    <div className="vector3"></div>
+                    <div className="vector4"></div>
+                    <div className="vector5"></div>
                 </div>
                 <div className="blurBottom2"></div>
             </div>
