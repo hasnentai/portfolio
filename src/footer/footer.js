cf1baa1904be3a579267202a8583f2e77c63041d
sumanaghosh15 <sumanaghosh822@gmail.com>
2021-11-12 20:41:29 +0530

    adding social icons in footer

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,7 +1,9 @@
+import { Instagram, Linkedin, Twitter, Youtube } from 'react-feather';
 import './footer.css';
 
 function Footer () {
     return (
+        <div>
         <div className="mainFooter">
             <div className="footer1">
                 <p>© 2021 your name</p>
@@ -17,6 +19,13 @@ function Footer () {
             <div className="footer3">
                 <p>Back up</p>
             </div>
+        </div>
+        <div className="socialIcons">
+            <Instagram/>
+            <Linkedin/>
+            <Youtube/>
+            <Twitter/>
+        </div>
         </div>
     );
 }
