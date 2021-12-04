c930fdb9e341879bc791a2f9a2e8aecc1f5d6af0
sumanaghosh15 <sumanaghosh822@gmail.com>
2021-12-04 15:57:36 +0530

    Header scrolling smooth

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,31 +1,43 @@
 import { Instagram, Linkedin, Twitter, Youtube } from 'react-feather';
 import './footer.css';
 
-function Footer () {
+function Footer() {
     return (
         <div>
-        <div className="mainFooter">
-            <div className="footer1">
-                <p>© 2021 your name</p>
+            <div className="mainFooter">
+                <div className="footer1">
+                    <p>© 2021 your name</p>
+                </div>
+                <div className="footer2">
+                    <ul>
+                        <li>Works</li>
+                        <li>About</li>
+                        <li>Articles</li>
+                        <li>Experiments</li>
+                    </ul>
+                </div>
+                <div className="footer3">
+                    <p>Back up</p>
+                </div>
             </div>
-            <div className="footer2">
-                <ul>
-                    <li>Works</li>
-                    <li>About</li>
-                    <li>Articles</li>
-                    <li>Experiments</li>
-                </ul>
+            <div className="socialIcons">
+                <a href="www.google.com">
+                    <Instagram />
+                    {/* <FontAwesomeIcon icon={faRandom} size="2x"/>           */}
+                </a>
+                <a href="www.google.com">
+                    <Linkedin />
+                </a>
+
+                <a href="www.google.com">
+                    <Youtube />
+                </a>
+
+                <a href="www.google.com">
+                    <Twitter />
+                </a>
+
             </div>
-            <div className="footer3">
-                <p>Back up</p>
-            </div>
-        </div>
-        <div className="socialIcons">
-            <Instagram/>
-            <Linkedin/>
-            <Youtube/>
-            <Twitter/>
-        </div>
         </div>
     );
 }
