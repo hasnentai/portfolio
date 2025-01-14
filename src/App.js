64f8abcb038214ee581087697b2f63869aff9fba
hasnentai <hasnen712@live.com>
2025-01-14 23:03:49 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   useEffect(() => {
     // Fetch the JSON file from the public folder
-    fetch("/assetlinks.json")
+    fetch("/.well-known/assetlinks.json")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
